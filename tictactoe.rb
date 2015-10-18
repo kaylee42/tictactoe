a82fb7185ef2ef5cab16d40a751dd97ce37236a2
kaylee42 <kayleeknowles42@gmail.com>
2015-10-18 11:08:57 -0400

    fixed to single program for both player, initial win recognize added

--- tictactoe.rb
+++ tictactoe.rb
@@ -12,11 +12,6 @@ end
 
 
 
-def ask_move
-  puts "Type the location you would like to mark."
-  display_board
-end
-
 @board = {"a1" => "A1", "a2" => "A2", "a3" => "A3", "b1" => "B1", "b2" => "B2", "b3" => "B3", "c1" => "C1", "c2" => "C2", "c3" => "C3"}
 @board_array = ["a1", "a2", "a3", "b1", "b2", "b3", "c1", "c2", "c3"]
 
@@ -26,36 +21,55 @@ def display_board
   puts "#{@board["c1"]} #{@board["c2"]} #{@board["c3"]}" 
 end
 
-def mark_board_X
-  puts "Player 1: Type the location you would like to mark."
+def mark_board(player)
+  puts "Player #{player}: Type the location you would like to mark."
   answer = gets.chomp
-  @board[answer.downcase] = "X "
+  @board[answer.downcase] = "#{player} "
   @board_array.delete(answer.downcase)
   display_board
 end
 
-def mark_board_O
-  puts "Player 2: Type the location you would like to mark."
-  answer = gets.chomp
-  @board[answer.downcase] = "O "
-  display_board
-end
 
 def mark_board_computer_easy
-  puts "Computer moves"
+  puts "Computer marks with an O"
   move = @board_array.sample
   @board[move] = "O "
   @board_array.delete(move)
   display_board
 end 
 
+def recognize_win(player)
+  win_array = []
+  @board.select {|key, value| win_array << key if value == "#{player} "}
+  if win_array.include?("a1") && win_array.include?("a2") && win_array.include?("a3")
+    true
+  elsif win_array.include?("b1") && win_array.include?("b2") && win_array.include?("b3")
+    true
+  elsif win_array.include?("c1") && win_array.include?("c2") && win_array.include?("c3")
+    true
+  elsif win_array.include?("a1") && win_array.include?("b1") && win_array.include?("c1")
+    true
+  elsif win_array.include?("a2") && win_array.include?("b2") && win_array.include?("c2")
+    true
+  elsif win_array.include?("a3") && win_array.include?("b3") && win_array.include?("c3")
+    true
+  elsif win_array.include?("a1") && win_array.include?("b2") && win_array.include?("c3")
+    true
+  elsif win_array.include?("a3") && win_array.include?("b2") && win_array.include?("c1")
+    true
+  else false
+  end 
+end
+
+
+
 def play_2_player
 display_board
 counter = 0
 while counter < 9
- mark_board_X
+ mark_board("X")
   counter+=1
-mark_board_O
+mark_board("O")
   counter+=1
 end
 puts "GAME OVER. Stalemate."
@@ -65,7 +79,7 @@ def play_computer_easy
   display_board
   counter = 0
 while counter < 9
- mark_board_X
+ mark_board(X)
   counter+=1
 mark_board_computer_easy
   counter+=1
@@ -73,4 +87,4 @@ end
 puts "GAME OVER. Stalemate."
 end
 
-play_computer_easy
+play_2_player
