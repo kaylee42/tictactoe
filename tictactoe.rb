e120661e2e08d79cd23215cdda66ced11bd4018a
kaylee42 <kayleeknowles42@gmail.com>
2015-10-18 12:03:22 -0400

    recognize win integrated, greeting selections added

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,17 +1,3 @@
-def greeting
-puts "Would you like to play Tic Tac Toe? (yes/no)"
-answer = gets.chomp.downcase
-if answer.include? "no"
-  puts "Fine, whatever."
-elsif answer.include? "yes"
-  puts "Great, let's play!"
-else 
-  puts "Sorry, I didn't understand your answer."
-end
-end
-
-
-
 @board = {"a1" => "A1", "a2" => "A2", "a3" => "A3", "b1" => "B1", "b2" => "B2", "b3" => "B3", "c1" => "C1", "c2" => "C2", "c3" => "C3"}
 @board_array = ["a1", "a2", "a3", "b1", "b2", "b3", "c1", "c2", "c3"]
 
@@ -58,7 +44,7 @@ def recognize_win(player)
   elsif win_array.include?("a3") && win_array.include?("b2") && win_array.include?("c1")
     true
   else false
-  end 
+  end  
 end
 
 
@@ -66,25 +52,61 @@ end
 def play_2_player
 display_board
 counter = 0
-while counter < 9
+loop do
  mark_board("X")
-  counter+=1
-mark_board("O")
-  counter+=1
-end
-puts "GAME OVER. Stalemate."
+ puts "GAME OVER. Player X wins!" if recognize_win("X") == true
+ break if recognize_win("X") == true 
+counter+=1
+puts "GAME OVER. Stalemate." if counter == 9
+break if counter == 9 
+  mark_board("O")
+  puts "GAME OVER. Player O wins!" if recognize_win("O") == true
+  break if recognize_win("O") == true 
+counter+=1
+end 
 end
 
 def play_computer_easy
   display_board
   counter = 0
-while counter < 9
- mark_board(X)
-  counter+=1
+loop do
+  mark_board("X")
+ puts "GAME OVER. You win!" if recognize_win("X") == true
+ break if recognize_win("X") == true 
+counter+=1
+puts "GAME OVER. Stalemate." if counter == 9
+break if counter == 9 
 mark_board_computer_easy
+uts "GAME OVER. Computer wins!" if recognize_win("O") == true
+  break if recognize_win("O") == true 
   counter+=1
 end
-puts "GAME OVER. Stalemate."
 end
 
-play_2_player
+def greeting
+puts "Would you like to play Tic Tac Toe? (yes/no)"
+answer = gets.chomp.downcase
+if answer.include? "no"
+  puts "Fine, whatever."
+elsif answer.include? "yes"
+  puts "Awesome!"
+  player_or_comp
+else 
+  puts "Sorry, I didn't understand your answer. Please try again."
+  greeting
+end
+end
+
+def player_or_comp
+  puts "Please type 1 to play the computer or 2 to play two-player."
+  answer = gets.to_i
+  if answer == 1
+    play_computer_easy
+  elsif answer == 2
+    play_2_player
+  else "Sorry, I didn't understand your answer. Please try again."
+    player_or_comp
+  end
+end
+
+greeting 
