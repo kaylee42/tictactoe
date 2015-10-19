4400b950f7ee4e64169d8b1423a65c81bc1251a6
kaylee42 <kayleeknowles42@gmail.com>
2015-10-19 15:44:22 -0400

    yield method integrated to comp & 2-player, notes added

--- tictactoe.rb
+++ tictactoe.rb
@@ -9,13 +9,14 @@ def display_board #just makes the board visible to user
   puts "#{@board["c1"]} #{@board["c2"]} #{@board["c3"]}" 
 end
 
-def mark_board(player) #accepts an argument of either X or O for 2-player, removes string from array for playing computer, marks players selection on board
+def mark_board(player) #accepts an argument of either X or O for 2-player, marks players selection on board
   puts "Player #{player}: Type the location you would like to mark."
   answer = gets.chomp.downcase
-  if @board_array.include?(answer)
+  if @board_array.include?(answer) #checks to make sure that the entry is valid and has not already been played
     @board[answer] = "#{player} "
-    @board_array.delete(answer)
+    @board_array.delete(answer) #removes play from array of available spots
     display_board
+    puts "PLAYER #{player} WINS! Game Over." if recognize_win("#{player}") == true
   else 
     puts "I'm sorry, either that was an invalid entry or that spot has already been taken. Please try again."
     mark_board(player)
@@ -23,12 +24,13 @@ def mark_board(player) #accepts an argument of either X or O for 2-player, remov
 end
 
 
-def mark_board_computer_easy 
+def mark_board_computer_easy #the easy level just has the computer select a random location from array of unplayed spots
 puts "Computer marks with an O"
   move = @board_array.sample
   @board[move] = "O "
   @board_array.delete(move)
   display_board 
+  puts "COMPUTER WINS! Game Over." if recognize_win("O") == true
 end
 
 
@@ -111,6 +113,7 @@ def mark_board_computer_hard
   @board_array.delete(move)
 end
 display_board
+puts "COMPUTER WINS! Game Over." if recognize_win("O") == true
 end
 
 
@@ -141,51 +144,27 @@ end
 
 
 
-def play_2_player #method for 2 player game
-  #ends if player wins or all nine places used
-  #again, feel it is unnecessarily long and repetitive - working on simplifying
+def play_game #runs the actual game, breaks if win recognized or board filled
 display_board
 counter = 0
 loop do
  mark_board("X")
- puts "PLAYER X WINS! Game Over." if recognize_win("X") == true
  break if recognize_win("X") == true 
 counter+=1
 puts "STALEMATE. Game Over" if counter == 9
 break if counter == 9 
-  mark_board("O")
-  puts "PLAYER O WINS! Game Over." if recognize_win("O") == true
+yield #yields to either #mark_board("O"), or easy/hard computer methods depending on choices made by user
   break if recognize_win("O") == true 
 counter+=1
 end 
 end
-
-def play_computer #nearly identical to 2 player method except uses #mark_board_computer method
-  #again, working to possibly simplify/combine methods as they are nearly the same
-  display_board
-  counter = 0
-loop do
-  mark_board("X")
- puts "YOU WIN! Game Over." if recognize_win("X") == true
- break if recognize_win("X") == true 
-counter+=1
-puts "STALEMATE. Game Over." if counter == 9
-break if counter == 9 
-yield #yields in #easy_or_hard to which play computer program is chosen
-uts "COMPUTER WINS! Game Over." if recognize_win("O") == true
-  break if recognize_win("O") == true 
-  counter+=1
-end
-end
-
 
 
 def greeting #initiates game with user - is the only method that needs to be called for all other methods to run
 puts "Would you like to play Tic Tac Toe? (yes/no)"
 answer = gets.chomp.downcase
 if answer.include? "no"
-  puts "Fine, whatever.
-   I didn't want to play with you anyway."
+  puts "Fine, whatever. I didn't want to play with you anyway."
 elsif answer.include? "yes"
   player_or_comp
 else 
@@ -200,7 +179,7 @@ def player_or_comp #allows user to choose to play either computer or another per
   if answer == 1
     easy_or_hard
   elsif answer == 2
-    play_2_player
+    play_game {mark_board("O")}
   else "Sorry, I didn't understand your answer. Please try again."
     player_or_comp
   end
@@ -210,9 +189,9 @@ def easy_or_hard #allows user to choose level to play
   puts "Please type 1 to play on easy or 2 to play on hard."
   answer = gets.to_i
   if answer == 1
-    play_computer {mark_board_computer_easy}
+    play_game {mark_board_computer_easy}
   elsif answer == 2
-    play_computer {mark_board_computer_hard}
+    play_game {mark_board_computer_hard}
   else "Sorry, I didn't understand your answer. Please try again."
     easy_or_hard
   end
